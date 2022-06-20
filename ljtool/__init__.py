8b63ff218b0cc82a8c3bd41509562482bd7211f7
xinglinjie <LJ_997@163.com>
2022-06-20 12:02:42 +0800

    亚马逊 评论日期转换 支持所有站点

--- ljtool/__init__.py
+++ ljtool/__init__.py
@@ -3,7 +3,7 @@
 # @Time    : 2022/6/20 10:09
 # @Author  : lj
 # @File    : __init__.py.py
-from .lj_p import *
-from .lj_headers import *
-from .lj_amazon import *
-from .lj_db import *
+import lj_amazon
+import lj_db
+import lj_headers
+import lj_p
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="ljtool",
-    version="0.0.2",
+    version="0.0.3",
     author="Linjie Xing",
     author_email="jin.951107@gmail.com",
     description="工具库",
